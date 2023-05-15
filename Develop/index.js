e45a58e6a33fc146d806036cfd4839e5fa121e74
Jaswinder Singh <jaswindersinghsaini@gmail.com>
2023-05-15 00:13:15 -0700

    pushing up the changes to main

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please Enter Title of your Project",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please Enter Project Description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please Enter Installation Requirements",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter how this can be used?",
+  },
+  {
+    type: 'list',
+    name: "license",
+    message: "Please choose a License from list",
+    choices: [
+      "MIT License",
+      "GNU Lesser General Public License v3.0",
+      "Mozilla Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "The Unlicense",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+    ],
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please Enter Contributors",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please Enter Tests if any?",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please Enter your github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please Enter your email",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (error) =>
+    error ? console.log(error) : console.log("Success!!")
+  );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    const markdownText = generateMarkdown(response);
+    writeToFile("Sample.md", markdownText);
+  });
+}
 
-// Function call to initialize app
+// // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,106 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Create an array with licences similar to choices
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const licenseArray = [
+  "MIT License",
+  "GNU Lesser General Public License v3.0",
+  "Mozilla Public License 2.0",
+  "GNU Affero General Public License v3.0",
+  "The Unlicense",
+  "Apache License 2.0",
+  "GNU General Public License v3.0",
+];
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+  if (license == licenseArray[0]) {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license == licenseArray[1]) {
+    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (license == licenseArray[2]) {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license == licenseArray[3]) {
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (license == licenseArray[4]) {
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+  } else if (license == licenseArray[5]) {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license == licenseArray[6]) {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license == licenseArray[0]) {
+    return `[${licenseArray[0]}](https://opensource.org/licenses/MIT)`;
+  } else if (license == licenseArray[1]) {
+    return `[${licenseArray[1]}](https://www.gnu.org/licenses/lgpl-3.0)`;
+  } else if (license == licenseArray[2]) {
+    return `[${licenseArray[2]}](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license == licenseArray[3]) {
+    return `[${licenseArray[3]}](https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if (license == licenseArray[4]) {
+    return `[${licenseArray[4]}](http://unlicense.org/)`;
+  } else if (license == licenseArray[5]) {
+    return `[${licenseArray[5]}](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license == licenseArray[6]) {
+    return `[${licenseArray[6]}](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else {
+    return "";
+  }
+}
+// // // TODO: Create a function that returns the license section of README
+// // // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+   ## Badges
+   ${renderLicenseBadge(data.license)}
+
+   ## Table of Contents
+   * [Description](#description)
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [License](#license)
+   * [Contributors](#contributors)
+   * [Tests](#tests)
+   * [Questions](#questions)
+
+   ## Description
+   ${data.description}
+
+
+   ## Installation
+   ${data.installation}
+
+   ## Usage
+   ${data.usage}
+
+   ## License
+   ${renderLicenseLink(data.license)}
+
+   ## Contributors
+   ${data.contributors}
+
+   ## Tests
+   ${data.tests}
+
+   ## Questions?
+   Github Profile:
+   [${data.username}](https://github.com/${data.username}) 
+
+
+   #For additional questions:
+   ${data.email}
+
+
+ `;
 }
 
 module.exports = generateMarkdown;
